4c44dd98ff6a3e683f44e257c5c5932e38ea925a
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2023-08-08 09:14:44 +0530

    first commit
    basic php read and write

---
+++ Assets/Scripts/Web.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Web : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/unitybackendtutorial/Getdate.php")) { 
+            yield return www.Send();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+
+
+}
